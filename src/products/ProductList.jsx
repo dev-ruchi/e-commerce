34ed057734b0c1209bebd0ad579b3eefec029409
Shivendra TechSter <shivendratechster@gmail.com>
2023-07-18 17:01:17 +0530

    fetch products and shows them

--- src/products/ProductList.jsx
+++ src/products/ProductList.jsx
@@ -1,9 +1,39 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+
 const ProductList = () => {
-    return(
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:8000/products')
+            .then(res => {
+                return res.json();
+            })
+            .then(data => {
+                setProducts(data);
+            })
+    }, [])
+
+    return (
         <div className="product-list">
-            <h2>Hello</h2>
+            {products.map(product => (
+                <div className="product-preview" key={product.id} >
+                    <Link to={`/products/${product.id}`}>
+                        <h2>{product.title}</h2>
+                        <p>{product.slug}</p>
+                        <p>{product.price}</p>
+                        <p>{product.description}</p>
+
+                    </Link>
+                </div>
+            ))
+
+            }
         </div>
     )
+
+
 }
 
 export default ProductList;
