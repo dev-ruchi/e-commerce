138b550898ee96170e2dedd7944ad4093ee0d93f
Shivendra TechSter <shivendratechster@gmail.com>
2023-07-22 22:26:30 +0530

    adds signup and login pages

--- src/products/ProductView.jsx
+++ src/products/ProductView.jsx
@@ -2,9 +2,12 @@ import { useParams } from "react-router-dom";
 
 const ProductView = () => {
     let { slug } = useParams();
+
+    
     return (
         <div className="products">
-            <h2>Products { slug }</h2>
+            <h2>Product{slug}</h2>
+          
         </div>
     )
 }
