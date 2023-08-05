c11f365d22cd4a55afefe9eaae11bc7df7194a30
Shivendra TechSter <shivendratechster@gmail.com>
2023-08-05 21:59:16 +0530

    Implement Sidebar transition

--- src/ui/Navbar.jsx
+++ src/ui/Navbar.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Menu, X } from "react-feather";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
@@ -14,29 +15,34 @@ const Navbar = () => {
     }
   ])
 
-  return (
-    <header className="p-4 md:px-16 md:py-4">
-      <nav className="p-4 absolute left-0 top-0 h-[100dvh] w-[100dvw] bg-gray-200 md:flex md:justify-between md:items-center">
-        <div className="flex justify-between">
-          <div className="md:hidden flex items-center gap-3 cursor-pointer">
-            <div className="nav-toggle">
-              <div className="line line-1"></div>
-              <div className="line line-2"></div>
-              <div className="line line-3"></div>
-            </div>
-            <a className="font-bold text-xl" href="/" title="Homepage">E-commerce</a>
-          </div>
-        </div>
+  const [opened, setOpened] = useState(false)
 
-        <div className="menu">
-          <div className="flex flex-col md:flex-row gap-1 md:gap-6">
-            {
-              menuItems.map(item => <Link key={item.link} to={item.link}>{item.label}</Link>)
-            }
+  function handleClick() {
+    setOpened(!opened);
+  }
+
+  return (
+    <>
+      <header className="flex justify-between py-4">
+        <div>
+          <div className="flex gap-2">
+            <Menu className="cursor-pointer" onClick={() => setOpened(true)} />
+            <div className="font-bold">
+              E-commerce
+            </div>
           </div>
         </div>
-      </nav>
-    </header>
+      </header>
+
+      <aside className={`transition-all ease-in-out duration-500 bg-gray-200 fixed top-0 h-screen w-screen ${opened ? 'left-0' : 'left-[-100%]'}`}>
+        <div className="flex gap-2 p-4">
+          <X className="cursor-pointer" onClick={() => setOpened(false)} />
+          <div className="font-bold">
+            E-commerce
+          </div>
+        </div>
+      </aside>
+    </>
   )
 }
 
