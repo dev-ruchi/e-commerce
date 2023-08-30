2c92d76904bf403805b653b3b72f82e0816c5a67
Shivendra TechSter <shivendratechSter@gmail.com>
2023-08-30 21:30:21 +0530

    Fix roundig in toPrice
    e.g. 11990 -> 12000

--- src/utils/number.js
+++ src/utils/number.js
@@ -8,9 +8,11 @@ export function toPrice(number, placeholder = "") {
 
     if (typeof number !== 'number') number = Number(number);
 
+    if (isNaN(number)) return placeholder;
+
     return Intl.NumberFormat(process.env.REACT_APP_LOCALE, {
-        maximumSignificantDigits: 2,
         style: "currency",
         currency: process.env.REACT_APP_CURRENCY,
+        minimumFractionDigits: 0,
     }).format(number);
 }
