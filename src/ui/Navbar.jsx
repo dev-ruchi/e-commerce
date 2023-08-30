8724bc9db8cd33e4f4a9e90eb7b9a64ea1ba73d0
Shivendra TechSter <shivendratechSter@gmail.com>
2023-08-30 23:39:01 +0530

    Render menu items

--- src/ui/Navbar.jsx
+++ src/ui/Navbar.jsx
@@ -1,46 +1,56 @@
 import { useState } from "react";
-import { Menu, X } from "react-feather";
+import { Home, LogOut, Menu, User, X } from "react-feather";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
 
-  const [menuItems, setMenuItems] = useState([
+  const [menuItems] = useState([
     {
       link: '/',
-      label: 'Home'
+      label: 'Home',
     },
     {
       link: '/account',
-      label: 'My Account'
-    }
+      label: 'My Account',
+    },
+    {
+      link: '/logout',
+      label: 'Logout',
+    },
   ])
 
-  const [opened, setOpened] = useState(false)
-
-  function handleClick() {
-    setOpened(!opened);
-  }
+  const [opened, setOpened] = useState(true)
 
   return (
     <>
       <header className="flex justify-between py-4">
         <div>
-          <div className="flex gap-2">
+          <div className="flex items-center gap-2">
             <Menu className="cursor-pointer" onClick={() => setOpened(true)} />
-            <div className="font-bold">
+            <div className="font-bold text-lg">
               E-commerce
             </div>
           </div>
         </div>
       </header>
 
-      <aside className={`transition-all ease-in-out duration-500 bg-gray-200 fixed top-0 h-screen w-screen ${opened ? 'left-0' : 'left-[-100%]'}`}>
-        <div className="flex gap-2 p-4">
+      <aside className={`p-4 transition-all ease-in-out duration-500 bg-white fixed top-0 h-screen w-screen ${opened ? 'left-0' : 'left-[-100%]'}`}>
+        <div className="flex items-center gap-2 mb-3">
           <X className="cursor-pointer" onClick={() => setOpened(false)} />
-          <div className="font-bold">
+          <div className="font-bold text-lg">
             E-commerce
           </div>
         </div>
+
+        <ul>
+          {menuItems.map(item => (
+            <li className="mb-3">
+              <Link to={item.link}>
+                {item.label}
+              </Link>
+            </li>
+          ))}
+        </ul>
       </aside>
     </>
   )
