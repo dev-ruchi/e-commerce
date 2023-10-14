1b84ff1420ff8d74f68a365454150fd5935c3af7
Shivendra TechSter <shivendratechster@gmail.com>
2023-10-14 19:41:02 +0530

    WIP: ESLint setup

--- src/products/ReviewForm.jsx
+++ src/products/ReviewForm.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Form from 'ui/Form';
 
 function ReviewForm() {
+    let x =  0;
     const fields = [
         { name: 'title', label: 'Title' },
         { name: 'body', label: 'Body', as: 'textarea' },
