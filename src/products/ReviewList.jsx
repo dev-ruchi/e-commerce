ef4a204b16cf98532ced7bd65f29d06b792f9348
Shivendra TechSter <shivendratechster@gmail.com>
2023-10-18 19:52:16 +0530

    integrate GET /reviews

--- src/products/ReviewList.jsx
+++ src/products/ReviewList.jsx
@@ -1,35 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
+import backend from "utils/backend";
 
 function ReviewList() {
-  const [reviews] = useState([
-    {
-      id: 1,
-      title: "Must buy",
-      body: "Lorem Ipsum Dolor Sit Amet",
-      user: {
-        name: "John Doe",
-        avatar: "https://placehold.co/40x40/png",
-      },
-    },
-    {
-      id: 2,
-      title: "Good product",
-      body: "Lorem Ipsum Dolor Sit Amet",
-      user: {
-        name: "Jane Doe",
-        avatar: "https://placehold.co/40x40/png",
-      },
-    },
-  ]);
+  const [reviews, setReviews] = useState([]);
+
+  useEffect(() => {
+    backend.get("/reviews").then((res) => setReviews(res.data));
+  }, []);
+
   return (
     <div>
       <h1 className="text-xl font-bold mb-4">Reviews</h1>
       {reviews.map((review) => (
         <div key={review.id} className="mb-3">
           <div className="flex items-center gap-2">
-            <img className=" rounded-full" src={review.user.avatar} />
-            <h2>{review.user.name}</h2>
+            <img
+              className=" rounded-full"
+              src="https://placehold.co/40x40/png"
+            />
+            <h2>Jane Doe</h2>
           </div>
           <h1 className="font-bold">{review.title}</h1>
           <p>{review.body}</p>
