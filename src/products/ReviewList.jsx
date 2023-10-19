aee83cd742dc284889a6be9daa7c694207dead9b
Shivendra TechSter <shivendratechster@gmail.com>
2023-10-19 20:29:22 +0530

    show review.user in review

--- src/products/ReviewList.jsx
+++ src/products/ReviewList.jsx
@@ -2,6 +2,10 @@ import React, { useEffect } from "react";
 import { useState } from "react";
 import backend from "utils/backend";
 
+function getAvatar(user) {
+  return user.avatar || `https://ui-avatars.com/api/?name=${user.name}`;
+}
+
 function ReviewList() {
   const [reviews, setReviews] = useState([]);
 
@@ -15,11 +19,8 @@ function ReviewList() {
       {reviews.map((review) => (
         <div key={review.id} className="mb-3">
           <div className="flex items-center gap-2">
-            <img
-              className=" rounded-full"
-              src="https://placehold.co/40x40/png"
-            />
-            <h2>Jane Doe</h2>
+            <img className=" rounded-full" src={getAvatar(review.user)} />
+            <h2>{review.user.name}</h2>
           </div>
           <h1 className="font-bold">{review.title}</h1>
           <p>{review.body}</p>
